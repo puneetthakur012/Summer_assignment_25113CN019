137759cafd2beb2eaa1843f8b2aea3400cda32a8
puneetthakur012 <25113cn019@glbitm.ac.in>
2026-06-03 18:14:02 +0530

    Add files via upload

--- q1.java
+++ q1.java
@@ -1,24 +1,21 @@
-import java.util.*;
-public class q1{
+import java.util.Scanner;
+
+public class q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        boolean isPrime = true;
-        if (n <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i <= n/2;i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num != 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
         }
-        if (isPrime) {
-            System.out.println(n + " is a prime number.");
-        } else {
-            System.out.println(n + " is not a prime number.");
-        }FI
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
     }
 }
